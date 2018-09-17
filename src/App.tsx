1ba2b05288fc1ff8e04642e88c66c4be3ae6c933
Spencer Rendon <sarendon@bellsouth.net>
2018-09-17 14:53:04 -0400

    View Workout Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import * as React from "react";
 import "./App.css";
 // import RegisterUser from "./components/register-user";
 // import Login from "./components/login";
-import NewWorkout from "./components/new-workout";
+// import NewWorkout from "./components/new-workout";
 import logo from "./logo.svg";
 import { store } from "./store";
 import { Provider } from "react-redux";
+import ViewWorkout from "./components/view-workout";
 
 class App extends React.Component {
   public render() {
@@ -16,7 +17,8 @@ class App extends React.Component {
             <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">Welcome to React</h1>
           </header>
-          <NewWorkout />
+          <ViewWorkout/>
+          {/* <NewWorkout /> */}
           <p className="App-intro">
             To get started, edit <code>src/App.tsx</code> and save to reload.
           </p>
